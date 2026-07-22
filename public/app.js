794a5ef022d1021d20e469c157cde75384bbf53e
Thanh Tho <trieuthanhtho28@gmail.com>
2026-07-22 14:15:08 +0700

    Cập nhật giao diện mới

--- public/app.js
+++ public/app.js
@@ -9476,7 +9476,11 @@ window.smartScrollToBottom = function () {
             window.scrollToBottomSmooth();
         } else {
             const messagesDiv = document.getElementById("messages");
-            if (messagesDiv) messagesDiv.scrollTop = messagesDiv.scrollHeight;
+            if (messagesDiv) {
+                requestAnimationFrame(() => {
+                    messagesDiv.scrollTop = messagesDiv.scrollHeight;
+                });
+            }
         }
     }
 };
