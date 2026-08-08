a98fb7e0c3d1bed2a15271b59a5441083a3976c3
Thanh Tho <trieuthanhtho28@gmail.com>
2026-08-08 16:41:23 +0700

    feat: Update iOS & Android launcher icons to new Chat Tho-Fi logo

--- flutter_frontend/build/web/flutter_bootstrap.js
+++ flutter_frontend/build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3358076438"
+    serviceWorkerVersion: "3484974369"
   }
 });
